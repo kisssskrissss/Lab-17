e34d0405b17466f122c39ac9524503cfc125a165
krist <krist@LAPTOP-HIQLHLL6>
2022-11-25 22:16:08 +0500

    Добавьте файлы проекта.

---
+++ Lab 17/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab_17
+{
+    class Account<T>
+    {
+        public T Num { get; set; }
+        public int Balance { get; set; }
+        public string Name { get; set; }
+        public string GetInfo()
+        {
+            return $"{Num} {Balance} {Name}";
+        }
+        public void Input()
+        {
+            Console.WriteLine("Введите номер счета");
+            string stringNum = Console.ReadLine();
+            Num = (T)Convert.ChangeType(stringNum, typeof(T));
+            Console.WriteLine("Введите баланс");
+            Balance = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите имя владельца счета");
+            Name = Console.ReadLine();
+
+        }
+        
+    }
+}
---
+++ Lab 17/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Lab_17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account<int> account1 = new Account<int>();
+            account1.Input();
+            Console.WriteLine(account1.GetInfo());
+
+            Account<int> account2 = new Account<int>();
+            account2.Input();
+            Console.WriteLine(account1.GetInfo());
+            Console.ReadKey();
+        }
+    }
+}
